b78ae973bf5ba54bcaa339e9a994b33c5f803fb0
HOST <sh-kin@mail.ru>
2021-02-14 18:35:55 +0300

    TODO:MVP

---
+++ app/src/main/java/com/shishkindenis/locationtracker_child/activities/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.shishkindenis.locationtracker_child.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shishkindenis.locationtracker_child.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
